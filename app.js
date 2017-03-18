8965bd351e0c6b7e7d0712e1f9cc099fac412b77
Shawn Crusvar <scrusvar@alterisgroup.net>
2017-03-18 18:56:18 -0400

    - Updating headers

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(bodyParser.urlencoded({
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
